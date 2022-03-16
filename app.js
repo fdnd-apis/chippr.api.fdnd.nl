bc08295f812a1f0e90218d2910822d75f693b68d
boudewijnbout <boudewijn_bout@outlook.com>
2022-03-16 12:25:39 +0100

    Added patch v0.1

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 const openapi = require("./docs/openapi.json");
-const cors = require("cors");
 
+// Routes
 const projectsRoute = require("./routes/Projects");
 
 app.use("/projects", projectsRoute);
-app.use(cors);
 
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
-  res.json({
-    message:
-      "Welcome to the Chippr API! Please use the resources below to expore this API.",
-    github: "https://github.com/fdnd-apis/chippr",
-    spec: "https://tribe.api.fdnd.nl/v1",
-    docs: "https://redocly.github.io/redoc/?url=https:%2F%2Ftribe.api.fdnd.nl%2Fv1",
-  });
+    res.json({
+        message:
+            "Welcome to the Chippr API! Please use the resources below to expore this API.",
+        github: "https://github.com/fdnd-apis/chippr",
+        spec: "https://tribe.api.fdnd.nl/v1",
+        docs: "https://redocly.github.io/redoc/?url=https:%2F%2Ftribe.api.fdnd.nl%2Fv1",
+    });
 });
 
 // Send openapi doc as json
 app.get("/v1", (req, res) => {
-  res.json(openapi);
+    res.json(openapi);
 });
 
 app.listen(port, () => {
-  console.log(`API Listening on port: ${port}`);
+    console.log(`API Listening on port: ${port}`);
 });
--- routes/Projects.js
+++ routes/Projects.js
@@ -10,37 +10,56 @@ let jsonParser = bodyParser.json();
 
 // Get all projects
 router.get("/", (req, res) => {
-  con().query("SELECT * FROM clients", function (err, result, fields) {
-    res.send(result);
-  });
+    con().query("SELECT * FROM clients", function (err, result, fields) {
+        res.send(result);
+    });
+});
+
+// Update existing project
+router.patch("/", jsonParser, function (req, res) {
+    let projectId = req.params.id;
+
+    let title = req.body.title;
+    let slug = req.body.slug;
+    let description = req.body.description;
+    let logo = req.body.logo;
+
+    console.log(result);
+
+    // Update form data to database
+    con().query(
+        `UPDATE clients SET title = ${title}, slug = ${slug}, description = ${description}, logo = ${logo} WHERE id = ${projectId}`,
+        function (err, result, fields) {
+            res.send(result);
+        });
 });
 
 // Store a new project
 router.post("/", jsonParser, function (req, res) {
-  console.log(req.body);
-  let title = req.body.title;
-  let slug = req.body.slug;
-  let description = req.body.description;
-  let logo = req.body.logo;
+    console.log(req.body);
+    let title = req.body.title;
+    let slug = req.body.slug;
+    let description = req.body.description;
+    let logo = req.body.logo;
 
-  // Insert form data into database
-  con().query(
-    `INSERT INTO clients (title, slug, description, logo) VALUES ("${title}", "${slug}", "${description}", "${logo}")`
-  );
+    // Insert form data into database
+    con().query(
+        `INSERT INTO clients (title, slug, description, logo) VALUES ("${title}", "${slug}", "${description}", "${logo}")`
+    );
 
-  res.send(req.body);
+    res.send(req.body);
 });
 
 // Get specific project according to clientId
 router.get("/:id", (req, res) => {
-  let clientId = req.params.id;
+    let projectId = req.params.id;
 
-  con().query(
-    "SELECT * FROM clients WHERE id =" + clientId,
-    function (err, result, fields) {
-      res.send(result);
-    }
-  );
+    con().query(
+        "SELECT * FROM clients WHERE id =" + projectId,
+        function (err, result, fields) {
+            res.send(result);
+        }
+    );
 });
 
 module.exports = router;
