a31a4a068bc27e359c8487ce43792c4ce1d4810b
ju5tu5 <justus@ju5tu5.nl>
2022-03-17 14:10:45 +0100

    Trying to fix patch

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-require("dotenv").config();
-const { apply } = require("body-parser");
-const express = require("express");
-const port = 3000;
-const openapi = require("./docs/openapi.json");
-const app = express();
+require('dotenv').config()
+const { apply } = require('body-parser')
+const express = require('express')
+const port = 3000
+const openapi = require('./docs/openapi.json')
+const app = express()
 
 // Routes
-const projectsRoute = require("./routes/Projects");
+const projectsRoute = require('./routes/Projects')
 
 module.exports = express()
-  .use("/projects", projectsRoute)
+  .use('/projects', projectsRoute)
   .use(express.json())
 
-  .get("/", (req, res) => {
+  .get('/', (req, res) => {
     res.json({
-      message:
-        "Welcome to the Chippr API! Please use the resources below to expore this API.",
-      github: "https://github.com/fdnd-apis/chippr",
-      spec: "https://tribe.api.fdnd.nl/v1",
-      docs: "https://redocly.github.io/redoc/?url=https:%2F%2Ftribe.api.fdnd.nl%2Fv1",
-    });
+      message: 'Welcome to the Chippr API! Please use the resources below to expore this API.',
+      github: 'https://github.com/fdnd-apis/chippr',
+      spec: 'https://tribe.api.fdnd.nl/v1',
+      docs: 'https://redocly.github.io/redoc/?url=https:%2F%2Ftribe.api.fdnd.nl%2Fv1',
+    })
   })
 
   // Send openapi doc as json
-  .get("/v1", (req, res) => {
-    res.json(openapi);
-  });
+  .get('/v1', (req, res) => {
+    res.json(openapi)
+  })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
